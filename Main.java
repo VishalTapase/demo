d98f2939240d46a135a27768724d18884df25e84
VishalTapase <43906326+VishalTapase@users.noreply.github.com>
2020-10-02 17:50:47 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ import java.util.*;
 //java
 class Main{
     public static void main(String[] args) {
-        int[] nums=new int[]{ 0, 8, 4, 12, 2};
-       int n=Arrays.binarySearch(nums,0,3,2);
+        int[] nums=new int[]{ 0, 8, 4, 12, 2, 15, 22};
+       int n=Arrays.binarySearch(nums,0,6,2);
         System.out.println(n);
     }
 
