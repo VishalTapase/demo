83ea43a00b5fce34ca9637a80c0402b651c0854a
OmDx <55055020+OmDx@users.noreply.github.com>
2020-10-06 19:04:32 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,20 @@
 package com.company;
 
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 //java
 class Main{
-    public static void main(String[] args) {
-        int[] nums=new int[]{ 0, 8, 4, 12, 2, 15, 22};
-       int n=Arrays.binarySearch(nums,0,6,2);
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        InputStreamReader isr  = new InputStreamReader(System.in);
+		BufferedReader br= new BufferedReader(isr);
+        String[] st  = br.readLine().split(" ");
+        int[] nums=new int[n];
+        for(int i = 0 ; i < n ; i++){
+            nums[i] = Integer.parseInt(st[i]);
+        }
+        int n=Arrays.binarySearch(nums,0,6,2);
         System.out.println(n);
     }
 
